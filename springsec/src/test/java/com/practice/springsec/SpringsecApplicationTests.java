7c40863e2cec4b7096a8b9bc8b89fd86b3a149d7
shiv420 <shiv.goods@gmail.com>
2024-11-01 13:30:30 +0530

    file committed.

---
+++ springsec/src/test/java/com/practice/springsec/SpringsecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.springsec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
