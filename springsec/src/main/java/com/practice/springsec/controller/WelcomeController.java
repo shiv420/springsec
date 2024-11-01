7ae9b1f292721bac4afe347c1eb07761e58559cb
shiv420 <shiv.goods@gmail.com>
2024-11-01 13:58:18 +0530

    New file added

---
+++ springsec/src/main/java/com/practice/springsec/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.practice.springsec.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome to Spring Security framework.";
+    }
+}
